c229efe9a023a3a1dca40466ce93abdd3e18a99b
IgorVVieira <igor.gutoo63@gmail.com>
2020-10-09 10:23:26 -0300

    Testes de integração de pacientes passando

--- api/__tests__/integrations/patient.test.js
+++ api/__tests__/integrations/patient.test.js
@@ -1,33 +1,30 @@
+require('../../src/database'); // Pega método estático init do Model 
+
 const Patient = require('../../src/models/Patient');
 
 describe('Patients', () => {
     it('should create patient', async () => {
         const patient = await Patient.create({
-            name: 'Igor Vitor Vieira',
-            age: 20,
-            cpf: '14697884620',
-            gender: 'MASCULINO',
+            name: 'Amanda Lopes Adriano',
+            age: 18,
+            cpf: '12345678945',
+            gender: 'FEMININO',
             health_condition: 'EM TRATAMENTO',
             locale: 'SUDESTE',
         });
 
-        console.log(patient);
-
-        expect(patient.age).toBe(20);
+        expect(patient.age).toBe(18);
     });
 
-    // it('should list all patients', async () => {
-    //     const patients = await Patient.findAll();
+    it('should list all patients', async () => {
+        const patients = await Patient.findAll();
 
-    //     console.log(patients);
+        expect(patients).toBe(patients);
+    });
 
-    //     expect(patients).toBe(patients !== null);
-    // });
+    it('should list one patient', async () => {
+        const patient = await Patient.findByPk(1);
 
-    // it('should list one patient', async () => {
-    //     const patient = await Patient.findByPk(1);
-    //     console.log(patient);
-
-    //     expect(patient).toBe(patient !== null);
-    // });
+        expect(patient.id).toBe(1);
+    });
 });
